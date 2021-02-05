f5deb8288636d802e1c28471314b88be679219a9
VijayBurugupalli <55851191+VijayBurugupalli@users.noreply.github.com>
2021-02-05 09:58:30 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,14 +2,13 @@ public class Insertion{
     public static void main(String[] args){
         int[] arr=new int[]{5,2,4,6,1,3,7,9,8};
         for(int j=1;j<arr.length;j++){
-            int temp;
+            int temp=arr[j];
             int i=j-1;
-            while(i>=0 && arr[i]>arr[i+1]){
-                temp=arr[i+1];
-                arr[i+1]=arr[i];
-                arr[i]=temp;
+            while(i>=0 && arr[i]>temp){
+                arr[i+1]=a[i];
                 i=i-1;
             }
+            a[i+1]=temp;
         }
         System.out.print("sorted:");
         for(int i=0;i<arr.length;i++){
